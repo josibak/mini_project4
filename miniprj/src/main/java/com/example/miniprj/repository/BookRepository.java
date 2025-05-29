83c0f5f68125eb528217896e2c9dab43d575cdda
josibak <gustlwh@naver.com>
2025-05-29 21:46:25 +0900

    rename

--- miniprj/src/main/java/com/example/miniprj/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.miniprj.repository;
-
-import com.example.miniprj.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
